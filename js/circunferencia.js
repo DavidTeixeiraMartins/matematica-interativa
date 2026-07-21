3a8fffbd8bcf9cc9dde16170e139a29a78b45967
DavidTeixeiraMartins <davidtmartins2@gmail.com>
2026-07-21 04:03:18 -0300

    Add Circunferencia class for SVG circle drawing

--- js/circunferencia.js
+++ js/circunferencia.js
@@ -1,1 +1,26 @@
+class Circunferencia {
+
+    constructor(centro, raio) {
+        this.centro = centro;
+        this.raio = raio;
+    }
+
+    desenhar(svg) {
+
+        let circulo = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "circle"
+        );
+
+        circulo.setAttribute("cx", this.centro.x);
+        circulo.setAttribute("cy", this.centro.y);
+        circulo.setAttribute("r", this.raio);
+
+        circulo.setAttribute("fill", "none");
+        circulo.setAttribute("stroke", "black");
+
+        svg.appendChild(circulo);
+    }
+
+}
 
