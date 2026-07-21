e2fd3bc080298f1018164d2ae57469f360913aa3
DavidTeixeiraMartins <davidtmartins2@gmail.com>
2026-07-21 04:07:16 -0300

    Add Lousa class for SVG object management

--- js/lousa.js
+++ js/lousa.js
@@ -1,1 +1,20 @@
+class Lousa {
 
+    constructor(svg) {
+        this.svg = svg;
+        this.objetos = [];
+    }
+
+
+    adicionar(objeto) {
+        this.objetos.push(objeto);
+        objeto.desenhar(this.svg);
+    }
+
+
+    limpar() {
+        this.svg.innerHTML = "";
+        this.objetos = [];
+    }
+
+}
